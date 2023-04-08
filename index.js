995414eadb5b16c5bbf06433c2c25542bc1ddf21
vuslatsena <vuslatsena23@gmail.com>
2023-04-08 22:39:46 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,10 @@
+const introduction = (name) => {
+  return `Hi, my name is ${name}.`;
+};
+const introductionWithLanguage = (name, language) => {
+  return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+};
+
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+  return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
